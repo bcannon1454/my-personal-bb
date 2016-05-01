02f7f4d3f2fb6578f883045cc3760db0cef65a5d
bcannon <bcannon@iastate.edu>
2016-05-01 12:40:57 -0500

    Added form to display sem:course info and add entries

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var app = angular.module('myApp', ['ngRoute']);
 
+var data = (function() { 
+	
+	var courses = [{semester: "Fall", course: "Calculus"},
+				{semester: "Fall", course: "Algorithms"},
+				{semester: "Fall", course: "Operating Systems"},
+				{semester: "Fall", course: "English"},
+				{semester: "Spring", course: "Programming"},
+				{semester: "Spring", course: "Dog Walking"},
+				{semester: "Spring", course: "Business"}];
+		
+	return {
+		
+		getClasses : function() {
+			return courses;
+		},
+
+		addClass : function(s, c) {
+			courses.push({semester: s, course: c});
+		}
+	}
+})();
+
 app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
@@ -14,7 +36,13 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('dashCtrl', function($scope) {
+	$scope.entries = data.getClasses();
 	
+	//Simulate storing information in a database
+	$scope.submit = function(s,c) {
+		data.addClass(s,c);
+		alert("You added a course! Semester: " + s  + " Course: " + c);
+	}
 });
 
 app.controller('homeCtrl', function($scope) {
