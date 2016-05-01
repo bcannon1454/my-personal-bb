004bb6e47c13ec83c3d2ba8ad7526e999a9c6ee4
bcannon <bcannon@iastate.edu>
2016-05-01 09:38:12 -0500

    Page is up and routing correctly finally

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
 
-    $routeProvider.when("/", {
+    $routeProvider.when("/home", {
         controller: "homeCtrl",
-        templateUrl: "home.html"
+        templateUrl: "homepage.html"
     });
 
-	$routeProvider.when("/home", {
-		controller: "homeCtrl",
-		templateUrl: "homepage.html"
+	$routeProvider.when("/dashboard", {
+		controller: "dashCtrl",
+		templateUrl: "dashboard.html"
 	});
+});
 
+app.controller('dashCtrl', function($scope) {
+	
 });
 
 app.controller('homeCtrl', function($scope) {
-
+	
 });
 
 app.controller('indexController', function($scope) {
