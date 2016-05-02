0ce179fd2eeac4210564beafa5914a280a3810fe
ejshack <ejshack@iastate.edu>
2016-05-01 22:18:35 -0500

    assignments and notes sections

--- app.js
+++ app.js
@@ -22,6 +22,45 @@ var data = (function() {
 	}
 })();
 
+var assignments = (function() { 
+	
+	var todo = [{name: "Lab 03", course: "201", duedate: "March 15"},
+				{name: "HW 02", course: "311", duedate: "March 12"},
+				{name: "Javascript", course: "315", duedate: "March 11"},
+				{name: "Frameworks", course: "214", duedate: "March 1"},
+				{name: "Lab 04", course: "324", duedate: "March 5"},
+				{name: "HW 04", course: "102", duedate: "March 25"},
+				{name: "Quiz 04", course: "307", duedate: "March 18"}];
+		
+	return {
+		
+		getAssignments : function() {
+			return todo;
+		},
+
+		addAssignment : function(n, c, d) {
+			todo.push({name: n, course: c, duedate: d});
+		}
+	}
+})();
+
+var notes = (function() { 
+	
+	var items = [{note: "We can use notes for the quiz on Wednesday"},
+				 {note: "Class was canceled Monday, March 2nd"}];
+		
+	return {
+		
+		getNotes : function() {
+			return items;
+		},
+
+		addNote : function(n) {
+			items.push({note: n});
+		}
+	}
+})();
+
 app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
@@ -33,6 +72,16 @@ app.config(function ($routeProvider) {
 		controller: "dashCtrl",
 		templateUrl: "dashboard.html"
 	});
+	
+	$routeProvider.when("/assignments", {
+		controller: "assignCtrl",
+		templateUrl: "assignments.html"
+	});
+	
+	$routeProvider.when("/notes", {
+		controller: "notesCtrl",
+		templateUrl: "notes.html"
+	});
 });
 
 app.controller('dashCtrl', function($scope) {
@@ -42,6 +91,26 @@ app.controller('dashCtrl', function($scope) {
 	$scope.submit = function(s,c) {
 		data.addClass(s,c);
 		alert("You added a course! Semester: " + s  + " Course: " + c);
+	}
+});
+
+app.controller('assignCtrl', function($scope) {
+	
+	$scope.entries = assignments.getAssignments();
+	
+	//Simulate storing information in a database
+	$scope.submit = function(n, c, d) {
+		assignments.addAssignment(n, c, d);
+		alert("You added an assignment: " + n  + " Course: " + c + " due on " + d);
+	}
+});
+
+app.controller('notesCtrl', function($scope) {
+	$scope.entries = notes.getNotes();
+	
+	//Simulate storing information in a database
+	$scope.submit = function(n) {
+		notes.addNote(n);
 	}
 });
 
